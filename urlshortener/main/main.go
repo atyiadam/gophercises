ebb696cf13dd19d54c6835539e7630a29ba149a8
Adam Atyi <atyiadam@gmail.com>
2025-07-20 16:06:53 +0200

    Add feature to accept YAML file as input

--- urlshortener/main/main.go
+++ urlshortener/main/main.go
@@ -1,22 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
-
+	"os"
 	"urlshortener"
 )
+
+var fileVar string
 
 func main() {
 	mux := defaultMux()
 
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
+	yaml, err := openYAML(fileVar)
+	if err != nil {
+		log.Fatalf("Could not open YAML file '%s': %v", fileVar, err)
+	}
 
 	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mux)
 	if err != nil {
@@ -26,10 +27,25 @@ func main() {
 	http.ListenAndServe(":8080", yamlHandler)
 }
 
+func openYAML(file string) ([]byte, error) {
+	data, err := os.ReadFile(file)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
 func defaultMux() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", hello)
 	return mux
+}
+
+func init() {
+	flag.StringVar(&fileVar, "file", "input.yaml", "YAML file for path to URL mappings")
+	flag.Parse()
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
